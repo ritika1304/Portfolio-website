eb5eb27858359561ac11adc594452ca43c36b473
ritika1304 <130042375+ritika1304@users.noreply.github.com>
2023-06-11 12:00:47 +0530

    commit Text

--- my-app/src/Components/HeroImage.js
+++ my-app/src/Components/HeroImage.js
@@ -1,11 +1,22 @@
 //import "./HeroImageStyle.css";
 import React from 'react'
+import IntroImg from "../assets/image1.jpg"
+import { Link } from 'react-router-dom'
 
 const HeroImage = () => {
   return (
     <div className="hero">
         <div className="mask">
+          <img className='into-img' src={IntroImg} alt='IntroImg'/>
             
+        </div>
+        <div className='content'>
+          <p>HI,I'M Er. Ritika Yadav.</p>
+          <h1>React Developer.</h1>
+          <div>
+            <Link to="/project" className='btn'>PROJECTS</Link>
+            <Link to="/contact" className='btn'>CONTACT</Link>
+          </div>
         </div>
     </div>
   )
